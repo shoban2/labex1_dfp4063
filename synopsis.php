76c82ae52d5789b1cbe6a7af6892471a3bcf89dc
shoban2 <shoban0707raj@gmail.com>
2021-09-02 16:15:40 +0800

    try edit index

---
+++ synopsis.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>book</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="synopsis.php">Synopsis</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="chapter.php">Chapter List</a></li>
+        </ul>
+      </nav>
+
+      <?php
+            $synopsis = 'Synopsis';
+            echo"<h1>$synopsis</h1>";
+            ?>
+      <br>
+      <h2>
+      In the Game of Thrones, you either win or you die. Nine noble families fight for control of the mythical land of Westeros. Political and sexual intrigue is pervasive. Robert Baratheon (Mark Addy), King of Westeros, asks his old friend, Lord Eddard Stark (Sean Bean), to serve as Hand of the King, or highest official.
+      </h2>
+
+  </body>
+</html>
