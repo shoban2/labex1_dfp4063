7b62f23ec5dc958947ce9eb543642708c8077642
shoban2 <shoban0707raj@gmail.com>
2021-09-02 13:13:54 +0800

    try edit index

---
+++ index.php
@@ -0,0 +1,20 @@
+  
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+        echo "<h2>A Game of Thrones</h2>";
+    ?>
+    <?php
+        echo "<h3>Game of Thrones is the first novel in A Song of Ice and Fire, a series of fantasy novels by the American author George R. R. Martin. It was first published on August 1, 1996. The novel won the 1997 Locus Award and was nominated for both the 1997 Nebula Award and the 1997 World Fantasy Award.
+     </h3>";
+    ?> 
+
+</body>
+</html>
